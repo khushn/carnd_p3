f38158bd77d7d94d591921b0600922a41ad12c8b
khushn <carnd@ip-10-148-153-217.ap-southeast-1.compute.internal>
2017-04-07 21:04:54 +0530

    Adding the scripts used to train over various training lap directories.

---
+++ training_script.sh
@@ -0,0 +1,27 @@
+MODEL=3
+python model.py ./recordings/driving_iram_1/ $MODEL < inp
+python model.py ./recordings/driving_manzood_1/ $MODEL < inp
+python model.py ./recordings/driving_minhal_1/ $MODEL < inp
+python model.py ./recordings/driving_iram_1/ $MODEL 1 < inp
+python model.py ./recordings/driving_manzood_1/ $MODEL 1 < inp
+python model.py ./recordings/driving_minhal_1/ $MODEL 1 < inp
+python model.py ./recordings/minhal_2/ $MODEL < inp
+python model.py ./recordings/minhal_2/ $MODEL 1 < inp
+python model.py ./recordings/minhal_3_opp/ $MODEL < inp
+python model.py ./recordings/minhal_3_opp/ $MODEL 1 < inp
+python model.py ./recordings/manzood_2/ $MODEL < inp
+python model.py ./recordings/manzood_2/ $MODEL 1 < inp
+python model.py ./recordings/manzood_3_opp/ $MODEL < inp
+python model.py ./recordings/manzood_3_opp/ $MODEL 1 < inp
+python model.py ./recordings/iram_2/ $MODEL < inp
+python model.py ./recordings/iram_2/ $MODEL 1 < inp
+python model.py ./recordings/iram_3_opp/ $MODEL < inp
+python model.py ./recordings/iram_3_opp/ $MODEL 1 < inp
+python model.py ./recordings/iram_4_opp2/ $MODEL < inp
+python model.py ./recordings/iram_4_opp2/ $MODEL 1 < inp
+python model.py ./recordings/minhal_4_opp2/ $MODEL < inp
+python model.py ./recordings/minhal_4_opp2/ $MODEL 1 < inp
+python model.py ./recordings/khush_try/ $MODEL < inp
+python model.py ./recordings/khush_try/ $MODEL 1 < inp
+#python model.py ./resources/data/ $MODEL < inp
+#python model.py ./resources/data/ $MODEL 1 < inp
---
+++ training_script_track2.sh
@@ -0,0 +1,11 @@
+MODEL=3
+python model.py ./recordings/man_min_track2/ $MODEL < inp
+python model.py ./recordings/man_min_track2/ $MODEL 1 < inp
+python model.py ./recordings/minhal_5_track2/ $MODEL < inp
+python model.py ./recordings/minhal_5_track2/ $MODEL 1 < inp
+python model.py ./recordings/iram_5_track2/ $MODEL < inp
+python model.py ./recordings/iram_5_track2/ $MODEL 1 < inp
+python model.py ./recordings/manzood_5_track2/ $MODEL < inp
+python model.py ./recordings/manzood_5_track2/ $MODEL 1 < inp
+python model.py ./recordings/manzood_6_track2/ $MODEL < inp
+python model.py ./recordings/manzood_6_track2/ $MODEL 1 < inp
